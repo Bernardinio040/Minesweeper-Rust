61d289bfec9f1a311941ba3657f2f677598441b8
Bernardinio040 <bernard.kaluzynski@gmail.com>
2025-12-18 15:38:01 +0100

    added template, working on topdown menu

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,74 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 use eframe::egui;
 
-#[derive(Default)]
-struct MyEguiApp {}
+pub struct Minesweeper {
+    label: String,
+    value: f32,
+}
 
-impl MyEguiApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        Self::default()
+impl Default for Minesweeper {
+    fn default() -> Self {
+        Self {
+            label: "Hello World!".to_owned(),
+            value: 2.7,
+        }
     }
 }
 
-impl eframe::App for MyEguiApp {
-   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-       egui::CentralPanel::default().show(ctx, |ui| {
-           ui.heading("Hello World!");
-       });
-      }
-   }
- 
-fn main   () {
-       println!("Hello, world!");
+impl Minesweeper {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Default::default()
+    }
 }
 
+impl eframe::App for Minesweeper {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        ctx.set_visuals(egui::Visuals::dark());
+        egui::TopBottomPanel::top("my_panel").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("Game", |ui| {
+                    #[cfg(not(target_arch = "wasm32"))]
+                    if ui.button("Quit").clicked() {
+                        ctx.send_viewport_cmd(ViewportCommand::Close);
+                    }
+                    //https://github.com/Seebass22/harptabber/blob/main/harptabber-gui/src/app.rs
+                    // ui.menu_item("Easy");
+                    // ui.menu_item("Medium");
+                    // ui.menu_item("Hard");
+                    // ui.menu_item("Exit");
+                    // if ui.menu_item("Exit").clicked() {
+                    //     ctx.send_viewport_cmd(ViewportCommand::Close);
+                    // }
+                });
+            })
+        });
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("eframe template");
+
+            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+
+            if ui.button("Increment").clicked() {
+                self.value += 1.0;
+            }
+        });
+    }
+}
+
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0])
+            .with_icon(
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon.png")[..])
+                    .expect("Failed to load icon"),
+            ),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Minesweeper!",
+        options,
+        Box::new(|cc| Ok(Box::new(Minesweeper::new(cc)))),
+    )
+}
