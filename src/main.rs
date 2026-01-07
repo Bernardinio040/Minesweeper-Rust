fcc4cdec2aa1e82e0b21e2e1e41a64c323868c0a
Bernardinio040 <bernard.kaluzynski@gmail.com>
2026-01-07 15:48:48 +0100

    added difficulties and bomb generation for medium and hard mode

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,21 @@ use crate::egui::ViewportCommand;
 use eframe::egui;
 use eframe::egui::Vec2;
 
-pub struct Minesweeper {}
+enum Difficulty {
+    Easy,
+    Medium,
+    Hard,
+}
+
+pub struct Minesweeper {
+    wizard: Difficulty,
+}
 
 impl Default for Minesweeper {
     fn default() -> Self {
-        Self {}
+        Self {
+            wizard: Difficulty::Easy,
+        }
     }
 }
 
@@ -26,38 +36,46 @@ impl eframe::App for Minesweeper {
                     #[cfg(not(target_arch = "wasm32"))]
                     if ui.button("Easy").clicked() {
                         //9x9 minefield
+                        self.wizard = Difficulty::Easy;
                         ctx.send_viewport_cmd(egui::ViewportCommand::InnerSize(Vec2::new(
                             330.0, 400.0,
                         )));
                     }
                     if ui.button("Medium").clicked() {
                         //16x16 minefield
+                        self.wizard = Difficulty::Medium;
                         ctx.send_viewport_cmd(egui::ViewportCommand::InnerSize(Vec2::new(
-                            550.0, 500.0,
+                            570.0, 650.0,
                         )))
                     }
                     if ui.button("Hard").clicked() {
                         //30x16 minefield
+                        self.wizard = Difficulty::Hard;
                         ctx.send_viewport_cmd(egui::ViewportCommand::InnerSize(Vec2::new(
-                            900.0, 500.0,
+                            1060.0, 650.0,
                         )))
                     }
                     if ui.button("Exit").clicked() {
                         ctx.send_viewport_cmd(ViewportCommand::Close);
                     }
-                    //https://github.com/Seebass22/harptabber/blob/main/harptabber-gui/src/app.rs
                 });
             })
         });
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.style_mut().spacing.button_padding = Vec2::new(5.0, 5.0);
 
+            let (rows, cols) = match self.wizard {
+                Difficulty::Easy => (9, 9),
+                Difficulty::Medium => (16, 16),
+                Difficulty::Hard => (16, 30),
+            };
+
             ui.vertical(|ui| {
-                for _y in 0..9 {
+                for _y in 0..rows {
                     ui.horizontal(|ui| {
                         ui.spacing_mut().item_spacing = Vec2::new(5.0, 5.0);
 
-                        for _x in 0..9 {
+                        for _x in 0..cols {
                             if ui.add_sized([30.0, 30.0], egui::Button::new("")).clicked() {}
                         }
                     });
@@ -72,7 +90,7 @@ fn main() -> eframe::Result {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([330.0, 400.0])
             .with_min_inner_size([330.0, 400.0])
-            .with_max_inner_size([900.0, 500.0])
+            .with_max_inner_size([1060.0, 650.0])
             .with_icon(
                 eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon.png")[..])
                     .expect("Failed to load icon"),
