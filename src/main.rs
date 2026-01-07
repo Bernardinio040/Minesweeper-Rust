809b027bfa46fe4484e680992bd00cc880a415a6
Bernardinio040 <bernard.kaluzynski@gmail.com>
2026-01-07 12:48:31 +0100

    added top-down settings for game difficulties

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+use crate::egui::ViewportCommand;
 use eframe::egui;
+use eframe::egui::Vec2;
 
 pub struct Minesweeper {
     label: String,
@@ -28,17 +30,28 @@ impl eframe::App for Minesweeper {
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("Game", |ui| {
                     #[cfg(not(target_arch = "wasm32"))]
-                    if ui.button("Quit").clicked() {
+                    if ui.button("Easy").clicked() {
+                        //9x9 minefield
+                        ctx.send_viewport_cmd(egui::ViewportCommand::InnerSize(Vec2::new(
+                            300.0, 400.0,
+                        )));
+                    }
+                    if ui.button("Medium").clicked() {
+                        //16x16 minefield
+                        ctx.send_viewport_cmd(egui::ViewportCommand::InnerSize(Vec2::new(
+                            550.0, 500.0,
+                        )))
+                    }
+                    if ui.button("Hard").clicked() {
+                        //30x16 minefield
+                        ctx.send_viewport_cmd(egui::ViewportCommand::InnerSize(Vec2::new(
+                            900.0, 500.0,
+                        )))
+                    }
+                    if ui.button("Exit").clicked() {
                         ctx.send_viewport_cmd(ViewportCommand::Close);
                     }
                     //https://github.com/Seebass22/harptabber/blob/main/harptabber-gui/src/app.rs
-                    // ui.menu_item("Easy");
-                    // ui.menu_item("Medium");
-                    // ui.menu_item("Hard");
-                    // ui.menu_item("Exit");
-                    // if ui.menu_item("Exit").clicked() {
-                    //     ctx.send_viewport_cmd(ViewportCommand::Close);
-                    // }
                 });
             })
         });
@@ -57,8 +70,9 @@ impl eframe::App for Minesweeper {
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])
+            .with_inner_size([300.0, 400.0])
+            .with_min_inner_size([250.0, 400.0])
+            .with_max_inner_size([900.0, 500.0])
             .with_icon(
                 eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon.png")[..])
                     .expect("Failed to load icon"),
